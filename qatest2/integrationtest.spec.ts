b9fc10ec8a26320bc91809d18f12904b3b89d654
Jittipat (Bam) Patcharadechathorn <jittipat.pa@CJDFintech.com>
2023-04-21 16:10:41 +0700

    test

--- qatest2/integrationtest.spec.ts
+++ qatest2/integrationtest.spec.ts
@@ -5,14 +5,15 @@ test.use({
 });
 
 test('test', async ({ page }) => {
+  // await page.setDefaultTimeout(30000);
   await page.goto('https://www.theguardian.com/international');
-  // await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
   await page.frameLocator('iframe[title="The Guardian consent message"]').getByRole('button', { name: 'Yes, I’m happy' }).click();
-  // await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
   await page.locator('#bannerandheader').getByRole('link', { name: 'Sport' }).click();
-  // await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
   await page.getByRole('button', { name: 'Close this banner' }).click();
-  // await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
   await page.locator('#bannerandheader').getByRole('link', { name: 'Tennis' }).click();
   await page.waitForTimeout(5000);
   await expect(page).toHaveScreenshot();
